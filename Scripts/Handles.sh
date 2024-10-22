596ba39137970207906b53de05bf993e70665c4e
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2024-10-22 21:31:04 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -4,8 +4,19 @@ PKG_PATCH="$GITHUB_WORKSPACE/wrt/package/"
 
 #预置OpenClash内核和数据
 if [ -d *"openclash"* ]; then
+
+	if echo "$WRT_TARGET" | grep -Eiq "64|86"; then
+	    CORE_TYPE="amd64"
+	elif echo "$WRT_TARGET" | grep -Eiq "IPQ807X"; then
+	    CORE_TYPE="arm64"
+	elif echo "$WRT_TARGET" | grep -Eiq "MT7621"; then
+	    CORE_TYPE="mipsle-softfloat"
+	else
+	    CORE_TYPE="$WRT_TARGET"  # 默认情况下返回WRT_TARGET的值
+	fi
+ 
+        #CORE_TYPE=$(echo $WRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo $WRT_TARGET | grep -Eiq "IPQ807X" && echo "arm64" || echo $WRT_TARGET | grep -Eiq "MT7621" && echo "mipsle-softfloat")
 	CORE_VER="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version"
-	CORE_TYPE=$(echo $WRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo $WRT_TARGET | grep -Eiq "IPQ807X" && echo "arm64" || echo $WRT_TARGET | grep -Eiq "MT7621" && echo "mipsle-softfloat")
 	CORE_TUN_VER=$(curl -sL $CORE_VER | sed -n "2{s/\r$//;p;q}")
 
 	CORE_DEV="https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux-$CORE_TYPE.tar.gz"
