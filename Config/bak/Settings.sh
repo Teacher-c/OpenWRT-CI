f55da628c5fdcafd8a58e3225001e43bd54ba050
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2024-10-22 21:34:19 +0800

    Update Settings.sh

--- Config/bak/Settings.sh
+++ Config/bak/Settings.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#删除冲突插件
-rm -rf $(find feeds/luci/ -type d -regex ".*\(argon\|design\|mosdns\|v2ray-geodata\|passwall\|openclash\).*")
-rm -rf $(find feeds/packages/ -type d -regex ".*\(argon\|design\|mosdns\|v2ray-geodata\|passwall\|openclash\).*")
-#rm -rf $(find feeds/smpackage/ -type d -regex ".*\(argon\|design\|mosdns\|v2ray-geodata\|passwall\|openclash\).*")
-#rm -rf $(find feeds/smpackage/ -type d -regex ".*\(base-files\|dnsmasq\|firewall\|fullconenat\|libnftnl\|nftables\|ppp\|opkg\|ucl\|upx\|vsftpd-alt\|miniupnpd-iptables\|wireless-regdb\).*")
-
 #curl supports h3/quic.
 # nghttp3
 rm -rf feeds/packages/libs/nghttp3
@@ -64,13 +58,6 @@ pushd package/libs/openssl/patches
     curl -sO https://raw.githubusercontent.com/sbwml/r4s_build_script/master/openwrt/patch/openssl/quic/0044-QUIC-Update-metadata-version.patch
 popd
   
-if [[ "$OWRT_URL" != *"NSS"* || "$OWRT_URL" != *"ipq"* ]]; then
-  
-  #添加turboacc
-  curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-            
-fi
-
 
 if [[ $OWRT_URL == *"openwrt"* ]] ; then
   
@@ -92,18 +79,4 @@ if [[ $OWRT_URL == *"openwrt"* ]] ; then
   #cp -rf $GITHUB_WORKSPACE/general/AX6/nss-status/immortal/luci-mod-status.json feeds/luci/modules/luci-mod-status/root/usr/share/rpcd/acl.d/
   #cp -rf $GITHUB_WORKSPACE/general/AX6/nss-status/luci_file_from_immortal/luci feeds/luci/modules/luci-base/root/usr/share/rpcd/ucode/
   echo 'skip'
-fi
-
-#修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-$OWRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
-#修改默认IP地址
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$OWRT_IP/g" ./package/base-files/files/bin/config_generate
-#修改默认主机名
-sed -i "s/hostname='.*'/hostname='$OWRT_NAME'/g" ./package/base-files/files/bin/config_generate
-#修改默认时区
-sed -i "s/timezone='.*'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
-sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
-if [[ $OWRT_URL == *"lede"* ]] ; then
-  #修改默认时间格式
-  sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
 fi
