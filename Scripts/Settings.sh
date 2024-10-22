298713e44e98ffcec07dd03aac573fe2ae13f922
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2024-10-22 21:13:22 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -22,3 +22,9 @@ echo "CONFIG_PACKAGE_luci=y" >> ./.config
 echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 #echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
+
+#添加turboacc
+if [[ "$WRT_TARGET" == *"86|64"* ]]; then
+  echo "CONFIG_PACKAGE_luci-app-turboacc=y" >> .config
+  curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+fi
