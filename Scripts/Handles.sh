a8b48ce4f1e21df2763350d1c984cd58a3446087
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2025-05-07 01:12:59 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -23,26 +23,21 @@ if [ -d *"openclash"* ]; then
 	#CORE_TUN="https://github.com/vernesong/OpenClash/raw/core/master/premium/clash-linux-$CORE_TYPE-$CORE_TUN_VER.gz"
         #CORE_DEV="https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-$CORE_TYPE.tar.gz"
 
+	cd ./luci-app-openclash/root/etc/openclash/
 	mkdir ./core/ && cd ./core/
-
 	curl -sL -o meta.tar.gz $CORE_MATE && tar -zxf meta.tar.gz && mv -f clash clash_meta && echo "meta done!"
 	#curl -sL -o tun.gz $CORE_TUN && gzip -d tun.gz && mv -f tun clash_tun && echo "tun done!"
 	#curl -sL -o dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz && echo "dev done!"
-
 	chmod +x ./* && rm -rf ./*.gz
-
         cd $PKG_PATCH && echo "openclash core has been updated!"
-        
+	
 	#GEO_MMDB="https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country-only-cn-private.mmdb"
 	GEO_SITE="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat"
 	GEO_IP="https://raw.githubusercontent.com/Loyalsoldier/geoip/release/geoip-only-cn-private.dat"
-
-	cd ./luci-app-openclash/root/etc/openclash/
-
+        cd ./luci-app-openclash/root/etc/openclash/
 	#curl -sL -o Country.mmdb $GEO_MMDB && echo "Country.mmdb done!"
 	curl -sL -o GeoSite.dat $GEO_SITE && echo "GeoSite.dat done!"
 	curl -sL -o GeoIP.dat $GEO_IP && echo "GeoIP.dat done!"
-
 	cd $PKG_PATCH && echo "openclash GeoDate has been updated!"
 fi
 
