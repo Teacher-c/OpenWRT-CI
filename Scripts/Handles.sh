13709b32680d596ddbc831df358235c95cfd8e19
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2025-09-05 11:13:57 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -15,13 +15,13 @@ if [ -d *"openclash"* ]; then
 	    CORE_TYPE="$WRT_TARGET"  # 默认情况下返回WRT_TARGET的值
 	fi
  
-        #CORE_TYPE=$(echo $WRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo $WRT_TARGET | grep -Eiq "IPQ807X" && echo "arm64" || echo $WRT_TARGET | grep -Eiq "MT7621" && echo "mipsle-softfloat")
+    #CORE_TYPE=$(echo $WRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo $WRT_TARGET | grep -Eiq "IPQ807X" && echo "arm64" || echo $WRT_TARGET | grep -Eiq "MT7621" && echo "mipsle-softfloat")
 	#CORE_VER="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/core_version"
 	#CORE_TUN_VER=$(curl -sL $CORE_VER | sed -n "2{s/\r$//;p;q}")
 
 	CORE_MATE="https://github.com/vernesong/OpenClash/raw/core/master/meta/clash-linux-$CORE_TYPE.tar.gz"
 	#CORE_TUN="https://github.com/vernesong/OpenClash/raw/core/master/premium/clash-linux-$CORE_TYPE-$CORE_TUN_VER.gz"
-        #CORE_DEV="https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-$CORE_TYPE.tar.gz"
+    #CORE_DEV="https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-$CORE_TYPE.tar.gz"
 
 	cd ./luci-app-openclash/root/etc/openclash/
 	mkdir ./core/ && cd ./core/
@@ -29,16 +29,24 @@ if [ -d *"openclash"* ]; then
 	#curl -sL -o tun.gz $CORE_TUN && gzip -d tun.gz && mv -f tun clash_tun && echo "tun done!"
 	#curl -sL -o dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz && echo "dev done!"
 	chmod +x ./* && rm -rf ./*.gz
-        cd $PKG_PATCH && echo "openclash core has been updated!"
+    cd $PKG_PATCH && echo "openclash core has been updated!"
 	
 	GEO_MMDB="https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country-only-cn-private.mmdb"
 	GEO_SITE="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat"
 	GEO_IP="https://raw.githubusercontent.com/Loyalsoldier/geoip/release/geoip-only-cn-private.dat"
-        cd ./luci-app-openclash/root/etc/openclash/
+    cd ./luci-app-openclash/root/etc/openclash/
 	curl -sL -o Country.mmdb $GEO_MMDB && echo "Country.mmdb done!"
 	curl -sL -o GeoSite.dat $GEO_SITE && echo "GeoSite.dat done!"
 	curl -sL -o GeoIP.dat $GEO_IP && echo "GeoIP.dat done!"
 	cd $PKG_PATCH && echo "openclash GeoDate has been updated!"
 fi
-
-
+#添加FakeSIP和FakeHTTP
+if [ -d *"openclash"* ]; then
+	FakeHTTP="https://github.com/MikeWang000000/FakeHTTP/releases/latest/download/fakehttp-linux-$CORE_TYPE.tar.gz"
+	FakeSIP="https://github.com/MikeWang000000/FakeSIP/releases/latest/download/fakesip-linux-$CORE_TYPE.tar.gz"
+    cd ./luci-app-openclash/root/etc/openclash/
+    curl -sL -o FakeHTTP.tar.gz $FakeHTTP && tar -zxf FakeHTTP.tar.gz && echo "FakeHTTP done!"
+	curl -sL -o FakeSIP.tar.gz $FakeSIP && tar -zxf FakeSIP.tar.gz && echo "FakeSIP done!"
+    chmod +x ./* && rm -rf ./*.gz
+	cd $PKG_PATCH && echo "FakeSIP FakeHTTP has been updated!
+fi
