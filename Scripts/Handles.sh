b8dc1cc8624fea177f56d2e457702928aca6b070
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2025-09-11 20:53:12 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -63,34 +63,24 @@ if [ -d *"openclash"* ]; then
 	cd $PKG_PATCH && echo "FakeSIP FakeHTTP has been updated!"
 fi
 
-#修改argon主题字体和颜色
+#修改argon主题字体
 if [ -d *"luci-theme-argon"* ]; then
-	echo " "
-
 	cd ./luci-theme-argon/
-
 	sed -i "/font-weight:/ { /important/! { /\/\*/! s/:.*/: var(--font-weight);/ } }" $(find ./luci-theme-argon -type f -iname "*.css")
-	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
-
+	sed -i "s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
 	cd $PKG_PATH && echo "theme-argon has been fixed!"
 fi
 
 #修改qca-nss-drv启动顺序
 NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
 if [ -f "$NSS_DRV" ]; then
-	echo " "
-
 	sed -i 's/START=.*/START=85/g' $NSS_DRV
-
 	cd $PKG_PATH && echo "qca-nss-drv has been fixed!"
 fi
 
 #修改qca-nss-pbuf启动顺序
 NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
 if [ -f "$NSS_PBUF" ]; then
-	echo " "
-
 	sed -i 's/START=.*/START=86/g' $NSS_PBUF
-
 	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
 fi
