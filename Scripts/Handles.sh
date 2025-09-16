452bcbd211ef5567e3a7152a764854b2b29f1f42
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2025-09-16 18:08:15 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -15,19 +15,12 @@ if [ -d *"openclash"* ]; then
 	    CORE_TYPE="$WRT_TARGET"  # 默认情况下返回WRT_TARGET的值
 	fi
  
-    #CORE_TYPE=$(echo $WRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo $WRT_TARGET | grep -Eiq "IPQ807X" && echo "arm64" || echo $WRT_TARGET | grep -Eiq "MT7621" && echo "mipsle-softfloat")
-	#CORE_VER="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/core_version"
-	#CORE_TUN_VER=$(curl -sL $CORE_VER | sed -n "2{s/\r$//;p;q}")
-
-	CORE_MATE="https://github.com/vernesong/OpenClash/raw/core/master/meta/clash-linux-$CORE_TYPE.tar.gz"
-	#CORE_TUN="https://github.com/vernesong/OpenClash/raw/core/master/premium/clash-linux-$CORE_TYPE-$CORE_TUN_VER.gz"
-    #CORE_DEV="https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-$CORE_TYPE.tar.gz"
-
+	#CORE_MATE="https://github.com/vernesong/OpenClash/raw/core/master/meta/clash-linux-$CORE_TYPE.tar.gz"
+    #换用samrt内核
+	CORE_MATE="https://github.com/vernesong/OpenClash/raw/core/master/smart/clash-linux-$CORE_TYPE.tar.gz"
 	cd ./luci-app-openclash/root/etc/openclash/
 	mkdir ./core/ && cd ./core/
 	curl -sL -o meta.tar.gz $CORE_MATE && tar -zxf meta.tar.gz && mv -f clash clash_meta && echo "meta done!"
-	#curl -sL -o tun.gz $CORE_TUN && gzip -d tun.gz && mv -f tun clash_tun && echo "tun done!"
-	#curl -sL -o dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz && echo "dev done!"
 	chmod +x ./* && rm -rf ./*.gz
     cd $PKG_PATCH && echo "openclash core has been updated!"
 	
