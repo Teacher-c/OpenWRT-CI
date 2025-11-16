cf4a2f7237f837690a3e19caf5cc55fc37555799
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2025-11-17 00:18:18 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -71,18 +71,18 @@ fi
 cd $PKG_PATCH
 
 #修改qca-nss-drv启动顺序
-#NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
-#if [ -f "$NSS_DRV" ]; then
-#	sed -i 's/START=.*/START=85/g' $NSS_DRV
-#	cd $PKG_PATCH && echo "qca-nss-drv has been fixed!"
-#fi
+NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
+if [ -f "$NSS_DRV" ]; then
+	sed -i 's/START=.*/START=85/g' $NSS_DRV
+	cd $PKG_PATCH && echo "qca-nss-drv has been fixed!"
+fi
 
 #修改qca-nss-pbuf启动顺序
-#NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
-#if [ -f "$NSS_PBUF" ]; then
-#	sed -i 's/START=.*/START=86/g' $NSS_PBUF
-#	cd $PKG_PATCH && echo "qca-nss-pbuf has been fixed!"
-#fi
+NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
+if [ -f "$NSS_PBUF" ]; then
+	sed -i 's/START=.*/START=86/g' $NSS_PBUF
+	cd $PKG_PATCH && echo "qca-nss-pbuf has been fixed!"
+fi
 
 
 #移除luci-app-attendedsysupgrade概览页面
